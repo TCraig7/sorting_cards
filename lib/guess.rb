91cb7ddce623f253079e93a4ff85ef699a844248
Tara Craig <tara.craig7@gmail.com>
2018-05-09 23:08:47 -0600

    Delete card method and add correct and feedback methods

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,16 +1,28 @@
 require "./lib/card"
 
 class Guess
-  attr_reader   :guess,
-                :value,
-                :suit
+  attr_reader   :response,
+                :card
 
-  def initialize(guess)
-    @guess  = guess
+  def initialize(response, card)
+    @response = response
+    @card  = card
   end
 
-  def card(value, suit)
-    @value  = value
-    @suit   = suit
+  def correct?
+    response = @response.split
+    if response[0] == card.value && response[2] == card.suit
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if correct? == true
+      "Correct!"
+    else
+      "Incorrect!"
+    end
   end
 end
